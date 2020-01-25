2b6a3588d07506d06e8ac02b51fd712419a24526
lamineAya <lamine.messaci@gmail.com>
2020-01-25 13:42:19 +0100

    Projet_2_Analysez les tests de l’application MagiC GitHub

---
+++ app/src/main/java/com/openclassrooms/magicgithub/model/User.java
@@ -0,0 +1,54 @@
+package com.openclassrooms.magicgithub.model;
+
+import androidx.annotation.Nullable;
+
+import java.util.Objects;
+import java.util.Random;
+
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
+
+public class User {
+
+    private String id;
+    private String login;
+    private String avatarUrl;
+
+    public User (String id, String login, String avatarUrl) {
+        this.id = id;
+        this.login = login;
+        this.avatarUrl = avatarUrl;
+    }
+
+    /**
+     * Generate random user
+     */
+    public static User random () {
+        return FAKE_USERS_RANDOM.get (new Random ().nextInt (FAKE_USERS_RANDOM.size ()));
+    }
+
+    // --- GETTERS ---
+    public String getId () {
+        return id;
+    }
+
+    public String getLogin () {
+        return login;
+    }
+
+    public String getAvatarUrl () {
+        return avatarUrl;
+    }
+
+    @Override
+    public boolean equals (@Nullable Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (!(obj instanceof User)) return false;
+        return (((User) obj).avatarUrl == this.avatarUrl && ((User) obj).login == this.login);
+    }
+
+    @Override
+    public int hashCode () {
+        return Objects.hash (login, avatarUrl);
+    }
+}
